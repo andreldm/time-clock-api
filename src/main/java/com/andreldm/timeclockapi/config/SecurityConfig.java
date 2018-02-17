950bc7cb4b57e6a743aaca1c80ba62c0d386daf6
Andre Miranda <andre42m@gmail.com>
2018-02-17 01:18:06 -0300

    Add basic authentication (issue #2)

---
+++ src/main/java/com/andreldm/timeclockapi/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.andreldm.timeclockapi.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        http.csrf().disable();
+        http.authorizeRequests().anyRequest().authenticated();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+            .withUser("rick").password("morty").roles("MULTIVERSE_MASTER");
+    }
+}
