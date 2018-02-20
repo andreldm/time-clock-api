7002f0b78ad327081c05cf707a88ad1e1c4fa107
Andre Miranda <andre42m@gmail.com>
2018-02-19 21:26:31 -0300

    Read users from properties files
    instead of hard-coded values

--- src/main/java/com/andreldm/timeclockapi/config/SecurityConfig.java
+++ src/main/java/com/andreldm/timeclockapi/config/SecurityConfig.java
@@ -1,14 +1,25 @@
 package com.andreldm.timeclockapi.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import java.util.Properties;
+
+/**
+ * Security related configurations.
+ */
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Value("classpath:users.properties")
+    private Resource users;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic();
@@ -19,8 +30,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .inMemoryAuthentication()
-            .withUser("rick").password("morty").roles("MULTIVERSE_MASTER");
+        Properties properties = new Properties();
+        properties.load(users.getInputStream());
+
+        auth.userDetailsService(new InMemoryUserDetailsManager(properties));
     }
 }
